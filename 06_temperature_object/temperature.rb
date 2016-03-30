3b1a75369df37cd1c824dd2de4b891e9f30d56cb
Guin <guinifer.k@gmail.com>
2016-03-30 18:02:22 -0400

    just finished temoperature object

--- 06_temperature_object/temperature.rb
+++ 06_temperature_object/temperature.rb
@@ -1,23 +1,44 @@
+require "pry-byebug"
+
 class Temperature
 
-attr_accessor :temp
+  attr_accessor :temp
 
-def initialize(temp)
-  @temp = temp
+  def initialize(temp)
+    @temp = temp
+  end
+
+  def to_celsius
+    return temp[:c] if temp.has_key? :c
+    (temp[:f]-32) * 5 / 9
+
+  end
+
+  def to_fahrenheit
+    return temp[:f] if temp.has_key? :f
+    temp[:c] * 9 / 5 + 32
+  end
+
+  def self.in_celsius(temp)
+    self.new c: temp
+  end
+
+  def self.in_fahrenheit(temp)
+    self.new f: temp
+  end
 end
 
-def to_celsius
-  temp.each_key {|system| system == :f ? (temp[system] - 32) * 5 / 9 : temp{system}}
+class Celsius < Temperature
+  def initialize(temp)
+    hash = {c: temp}
+    @temp = hash
+  end
 end
 
-def to_fahrenheit
-  temp.each_key {|system| system == :c ? temp[system] * 9/5 +32 : temp{system} }
-end
+class Fahrenheit < Temperature
+  def initialize(temp)
+    hash = {f: temp}
+    @temp = hash
+  end
 
-def self.to_celsius
-  temp.each_key {|system| system == :f ? (temp[system] - 32) * 5 / 9 : temp{system}}
-end
-
-def self.to_fahrenheit
-  temp.each_key {|system| system == :c ? temp[system] * 9/5 +32 : temp{system} }
 end
