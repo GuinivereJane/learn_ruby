d5947e70b1939cfecc46d1207e264a7fd2265132
Guin <guinifer.k@gmail.com>
2016-03-30 22:30:12 -0400

    about to refactor

--- stretch_01_make_change/changer.rb
+++ stretch_01_make_change/changer.rb
@@ -7,17 +7,32 @@ class Changer
   @pennies
   @change = []
 
-  def dimes
-    temp = @pennies /10
-    temp.flooe
-    temp.times {@change << 10}
+  def self.coppers
+    @pennies.times {@change << 1}
     done
   end
-  
+
+  def self.nickels
+    temp = @pennies / 5
+    @pennies = @pennies % 5
+    temp.floor
+    temp.times {@change << 5}
+    coppers
+  end
+
+
+  def self.dimes
+    temp = @pennies / 10
+    @pennies = @pennies % 10
+    temp.floor
+    temp.times {@change << 10}
+    nickels
+  end
+
 
   def self.quarters
     temp = @pennies / 25
-    pennies = @pennies mod 25
+    @pennies = @pennies % 25
     temp = temp.floor
     temp.times {@change << 25}
     dimes
